8ae04c4dede57b2e37aa247c036aae95febd7c66
Tara <hymerstara@gmail.com>
2024-02-29 22:26:01 -0700

    fetch tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,20 +4,9 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const tweetInfo = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
-  },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
-  },
-  created_at: 1461116232227,
-};
-
 function createTweetElement(tweet) {
-  // Creating elements using jQuery
+  const timeAgo = timeago.format(tweet.posted);
+
   const $tweet = $(`
         <article class="tweet">
             <header>
@@ -29,6 +18,7 @@ function createTweetElement(tweet) {
                 <p>${tweet.content.text}</p>
             </div>
             <footer>
+             <span class="timestamp">${timeAgo}</span>
                 <div class="icons">
                     <i class="fas fa-flag"></i>
                     <i class="fas fa-retweet"></i>
@@ -44,6 +34,43 @@ function createTweetElement(tweet) {
 const renderTweets = function (tweets) {
   for (let tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    $("#tweets-container").prepend($tweet);
+    $("#tweet-container").prepend($tweet);
   }
 };
+
+//ajax to listen, prevent, serialize and post
+const submit = (event) => {
+  event.preventDefault();
+  const formData = $(event.target).serialize();
+  $.ajax({
+    url: "/tweets",
+    type: "POST",
+    data: formData,
+    success: function (response) {
+      console.log("Tweetered:", response);
+    },
+    error: function (xhr, status, error) {
+      console.error("Error:", error);
+    },
+  });
+};
+$("form").submit(handleSubmit);
+
+// AJAX GET request to fetch tweets
+const loadTweets = () => {
+  $.ajax({
+    url: "/tweets",
+    type: "GET",
+    dataType: "json",
+    success: function (response) {
+      renderTweets(response);
+    },
+    error: function (xhr, status, error) {
+      console.error("No tweets:", error);
+    },
+  });
+};
+
+$(document).ready(function () {
+  loadTweets();
+});
